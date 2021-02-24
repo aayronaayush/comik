ad99a7ba39e29038b0427546b35d99a8e6b32cfc
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-02-24 10:14:26 +0400

    add placeholder images for panels, add script and css file

---
+++ scripts/index.js
@@ -0,0 +1,17 @@
+let panelContainers = document.getElementsByClassName("panel")
+let panelContainersArray = []
+const keyPressedHandler = (e) => {
+    const num1KeyCode = 49
+    const panel = panelContainersArray[e.keyCode - num1KeyCode]
+    // panel.scrollIntoView()
+    console.log(panel.getBoundingClientRect().top)
+}
+
+document.addEventListener("keypress",keyPressedHandler)
+
+Object.keys(panelContainers).forEach(panelContainerKey => {
+    console.log(window.innerHeight)
+    let panel = panelContainers[panelContainerKey]
+    panel.style.height = window.innerHeight+"px"
+    panelContainersArray.push(panel)
+})
