e6111da43721ea374e4f6827ba1d3a7377636c72
Aayush Aayron Deo <aayushaayron@gmail.com>
2021-03-01 11:14:16 +0400

    fixed scrolling jitter

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,53 @@
 let panelContainers = document.getElementsByClassName("panel")
 let panelContainersArray = []
+let panel = null
+let currInterval = -1
+let yCoord
+
 const keyPressedHandler = (e) => {
     const num1KeyCode = 49
-    const panel = panelContainersArray[e.keyCode - num1KeyCode]
-    panel.scrollIntoView()
-    console.log(panel.getBoundingClientRect().top1)
+    panel = panelContainersArray[e.keyCode - num1KeyCode]
+    yCoord = panel.getBoundingClientRect().top
+    
+    // if there currently an interval set remove it
+    if (currInterval!=-1){
+        clearInterval(currInterval)
+        currInterval = -1
+    }
+
+    if (yCoord > 0){ // is the panel is not at the top
+        currInterval = setInterval(() => {
+            console.log(panel.getBoundingClientRect().top)
+            if (panel.getBoundingClientRect().top>=0) {
+                if (panel.getBoundingClientRect().top<10) {
+                    window.scrollBy(0,panel.getBoundingClientRect().top)
+                    clearInterval(currInterval)
+                    currInterval = -1
+                }
+                window.scrollBy(0,10)
+            } else {
+                clearInterval(currInterval)
+                currInterval = -1
+            }    
+        }, 10)
+    } else if(yCoord < 0){
+        currInterval = setInterval(() => {      
+            console.log(panel.getBoundingClientRect().top)     
+            if (panel.getBoundingClientRect().top<=0) {
+                if (panel.getBoundingClientRect().top>-10) {
+                    window.scrollBy(0,panel.getBoundingClientRect().top)
+                    clearInterval(currInterval)
+                    currInterval = -1
+                }
+                window.scrollBy(0,-10)
+            } else {
+                clearInterval(currInterval)
+                currInterval = -1
+            }    
+        }, 10)
+        // console.log("panel is above")
+    }
+    // panel.scrollIntoView()
 }
 
 document.addEventListener("keypress",keyPressedHandler)
